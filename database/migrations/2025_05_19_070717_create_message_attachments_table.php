80aad2949b36da5dce577c764f3bb70579ed9e0b
Panuwit <panuwit_89@hotmail.com>
2025-05-27 15:16:34 +0700

    make migrations

---
+++ database/migrations/2025_05_19_070717_create_message_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('message_id');
+            $table->string('path');
+            $table->string('original_name');
+            $table->string('mime_type');
+            $table->timestamps();
+
+            $table->foreign('message_id')
+                ->references('id')
+                ->on('messages')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message_attachments');
+    }
+};
