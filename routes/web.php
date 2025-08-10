bbaabfff7a63ec0ae096d20b925938db06334d19
Panuwit Sungthoop <panuwit.sun@ku.th>
2025-08-10 18:05:22 +0700

    change default url to login page

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->name('auth.google.redirect');
