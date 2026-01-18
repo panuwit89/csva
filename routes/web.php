803016208ffc8885a26b167446df8647b3194718
Panuwit Sungthoop <panuwit.sun@ku.th>
2026-01-18 15:51:06 +0700

    add delete endpoint

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::middleware(['auth'])->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::delete('conversation/multipleDelete', [ConversationController::class, 'multipleDelete'])->name('conversation.multipleDelete');
     Route::resource('conversation', ConversationController::class);
 
     Route::get('/knowledge/{knowledge}/download', [KnowledgeController::class, 'download'])->name('knowledge.download');
