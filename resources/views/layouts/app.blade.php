14bf0077253480e94d2cb91033d082c85e5f9745
Panuwit Sungthoop <panuwit.sun@ku.th>
2026-02-11 20:26:22 +0700

    change app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'PER.AI') }}</title>
+
+        <link rel="icon" href="{{ asset('storage/app/public/favicon.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
